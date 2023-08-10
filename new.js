fe6a9afdc4464348a1e5c3e400f94090dadb920e
Parth Modi <parthmodi22500@gmail.com>
2023-08-10 13:44:58 +0530

    serversidelogin

--- new.js
+++ new.js
@@ -1,66 +1,102 @@
-// // index.js (your backend entry file)
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const user = require('./userdetails'); // Assign the result of require to a variable
-const app = express(); // Create an instance of Express
+  // // index.js (your backend entry file)
+  const express = require('express');
+  const cors = require('cors');
+  const mongoose = require('mongoose');
+  const user = require('./userdetails'); // Assign the result of require to a variable
+  const app = express(); // Create an instance of Express
+  const Jwt = require('jsonwebtoken');
+  const JwtKey='E-commerce';
+  app.use(express.json());
+  app.use(cors());
 
-app.use(express.json());
-app.use(cors());
+  const mongourl =
+    'mongodb+srv://pmodi0705:pymjNfFR30tPIIZr@cluster0.1kls1uz.mongodb.net/?retryWrites=true&w=majority';
+  mongoose
+    .connect(mongourl, {
+      useNewUrlParser: true,
+    })
+    .then(() => {
+      console.log('Connected to MongoDB');
+    })
+    .catch((e) => console.log('Error connecting to MongoDB:', e));
 
-const mongourl =
-  'mongodb+srv://pmodi0705:pymjNfFR30tPIIZr@cluster0.1kls1uz.mongodb.net/?retryWrites=true&w=majority';
-mongoose
-  .connect(mongourl, {
-    useNewUrlParser: true,
-  })
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((e) => console.log('Error connecting to MongoDB:', e));
+  // const User =mongoose.model("UserInfo");
 
-// const User =mongoose.model("UserInfo");
-
-app.post('/register', async (req, res) => {
-  const { username, password } = req.body;
-  const User = new user({ username, password }); // Use the user model from the userdetails module
-  const uname = await user.findOne({ username });
-
-  if (!uname) {
-    User.save()
-      .then(() => {
-        console.log('User added');
-        return res.json({ msg: 'User registered successfully' });
-      })
-      .catch((e) => {
-        console.log('Error adding a user ', e);
-        return res.status(500).json({ msg: 'Internal server error' });
+  app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+  
+    try {
+      const existingUser = await user.findOne({ username });
+  
+      if (existingUser) {
+        return res.status(400).json({ msg: 'User already exists' });
+      }
+  
+      const newUser = new user({ username, password });
+      await newUser.save();
+  
+      Jwt.sign({ username }, JwtKey, (err, token) => {
+        if (err) {
+          console.error('Error signing token:', err);
+          return res.status(500).json({ msg: 'Token signing error' });
+        }
+        
+        const userObj = { username }; 
+        res.json({ userObj, auth: token });
       });
-  } else {
-    console.log('User already exists');
-    return res.status(502).json({ msg: 'User already exists' });
-  }
-});
-
-// Route handler for user login (/login)
-app.post('/login', async (req, res) => {
-  const { username, password } = req.body;
- 
-  try {
-    const userObj = await user.findOne({ username });
-    if (!userObj || userObj.password !== password) {
-      return res.status(401).json({ msg: 'Invalid credentials //' });
+  
+    } catch (error) {
+      console.error('Error adding a user:', error);
+      res.status(500).json({ msg: 'Internal server error' });
     }
+  });
+  // for login 
+  app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+  
+    try {
+      const userObj = await user.findOne({ username });
+      if (!userObj || userObj.password !== password) {
+        return res.status(401).json({ msg: 'Invalid credentials //' });
+      }
+      Jwt.sign({ userObj }, JwtKey, (err, token) => {
+        if (err) {
+          console.error('Error signing token:', err);
+          return res.status(500).json({ msg: 'Token signing error' });
+        }
+        res.send({userObj,auth:token})
+        // return res.json({ msg: 'Successfully signed in', token });
+      });
+      
+      
+    } catch (error) {
+      console.error('Error signing in:', error);
+      return res.status(500).json({ msg: 'Internal server error' });
+    }
+  });
+  app.post('/add-visited', async (req, res) => {
+    const { username, visitedItem } = req.body;
+  
+    try {
+      const userObj = await user.findOne({ username });
+  
+      if (!userObj) {
+        return res.status(404).json({ msg: 'User not found' });
+      }
+  
+      userObj.recentlyVisited.push(visitedItem);
+      await userObj.save();
+  
+      res.json({ msg: 'Visited item added successfully' });
+    } catch (error) {
+      console.error('Error adding visited item:', error);
+      res.status(500).json({ msg: 'Internal server error' });
+    }
+  });
+  
 
-    return res.json({ msg: 'Successfully signed in' });
-  } catch (error) {
-    console.error('Error signing in:', error);
-    return res.status(500).json({ msg: 'Internal server error' });
-  }
-});
+  // ... (Start the server and other necessary code)
 
-// ... (Start the server and other necessary code)
-
-app.listen(5000, () => {
-  console.log('Server started on http://localhost:5000');
-});
+  app.listen(5000, () => {
+    console.log('Server started on http://localhost:5000');
+  });
--- userdetails.js
+++ userdetails.js
@@ -1,16 +1,20 @@
-const mongoose=require('mongoose')
+const mongoose = require('mongoose');
 
-const userSchema=new mongoose.Schema({
-    username:{
-        type:String,
-        required:true
-    },
-    password:{
-        type:String,
-        required:true
-    },
-    
-})
-const user=mongoose.model('user',userSchema)
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  recentlyVisited: {
+    type: [String], // Array of strings to store visited item names
+    default: [],    // Default to an empty array
+  },
+});
 
-module.exports=user 
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
